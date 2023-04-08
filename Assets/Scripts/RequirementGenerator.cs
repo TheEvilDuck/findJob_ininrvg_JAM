e67f50971855bf89fbf32ce25290436611f01fb3
TheEvilDuck <49678435+TheEvilDuck@users.noreply.github.com>
2023-04-08 13:14:32 +1000

    Initial commit

---
+++ Assets/Scripts/RequirementGenerator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class RequirementGenerator
+{
+    public static void GerenatreRequirement()
+    {
+        StreamReader streamReader = new StreamReader(Application.persistentDataPath+"/Texts/Requirements.txt");
+        
+    }
+}
